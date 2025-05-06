715515774319ef5de4cd1e950cda3bc0e6c7fe8a
Hazelanai <00050125@uca.edu.sv>
2025-05-06 10:38:42 -0600

    Add files via upload

---
+++ clase_00050125.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(void){
+    int n; 
+
+    cout <<"Escribe mi edad :\n";
+    cin >> n;
+
+    if(n < 18){
+        cout <<"Aun es menor de edad \n";
+
+    }else 
+    cout <<"Ya es mayor de edad";
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
