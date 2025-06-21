cbd5a0c86f3ccca99904cb607b4159ec827774dd
lucianavarro25 <00109525@uca.edu.sv>
2025-06-20 19:32:01 -0600

    DO WHILE EJERCICIO UNO

---
+++ ew1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int contador = 1;
+    // Ciclo while
+    while (contador <= 10) {
+        cout << "Contador en while: " << contador << endl;
+        contador++; // Incrementa el contador
+    }
+
+    return 0;
+}
