b08a837c48c134d1b61e57305f79e458b19fc979
lucianavarro25 <00109525@uca.edu.sv>
2025-06-20 22:03:01 -0600

    DO WHILE 3

--- ew3.cpp
+++ ew3.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int main() {
+    int numeroSecreto = 42;  // Numero fijo para adivinar
+    int intento = 0;
+
+    cout << "Adivina el numero entre 1 y 100.\n";
+
+    while (true) {
+        cout << "Introduce tu intento: ";
+        cin >> intento;
+
+        if (intento == numeroSecreto) {
+            cout << "¡Felicidades! Adivinaste el numero.\n";
+            break;
+        }
+        else if (intento < numeroSecreto) {
+            cout << "El numero es mayor. Intenta de nuevo.\n";
+        }
+        else {
+            cout << "El numero es menor. Intenta de nuevo.\n";
+        }
+    }
+
     return 0;
 }
